5fcfdfc1ed850e6f390404c19171a89daf4a35c7
JHallPD <jeffhall94@gmail.com>
2019-06-29 20:52:26 -0400

    readded sign up email varification
    added verification host

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -56,7 +56,7 @@ class Firebase {
 
     doSendEmailVerification = () =>
         this.auth.currentUser.sendEmailVerification({
-            //url: process.env.REACT_APP_CONFIRMATION_EMAIL_REDIRECT,
+            url: process.env.REACT_APP_CONFIRMATION_EMAIL_REDIRECT,
         });
 
     doPasswordUpdate = password =>
